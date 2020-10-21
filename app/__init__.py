d0f2cb6f5657196f1ab25002c0fae00492ebc2ce
Pedro Henrique Altoé <phaltoe85@gmail.com>
2020-10-21 14:08:39 -0300

    geoapi on

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+"""
+Example RESTful API Server.
+"""
+from flask import Flask
+from flask_restplus import Api
+
+api_v1 = Api(
+    version='1.0',
+    title="FLASK | FLASK-RESTPlus GeoAPI",
+    description=(
+        "This is a FLASK-RESPlus powered API with geospatial super power.\n\n"
+        "Checkout more at https://gis-ops.com or https://github.com/gis-ops\n"
+    ),
+)
+
+def create_app(flask_config_name=None, **kwargs):
+    """
+    Entry point to the Flask RESTful Server application.
+    """
+
+    # Initialize the Flask-App
+    app: Flask = Flask(__name__, **kwargs)
+
+    # Load the config file
+    app.config.from_object('config.DevelopmentConfig')
+
+    # Initialize FLASK-RESTPlus
+    api_v1.init_app(app)
+
+    # Initialize the modules
+    from . import modules
+    modules.init_app(app)
+
+    return app
---
+++ config.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+class BaseConfig(object):
+    ENABLED_MODULES = {
+        'api',
+        'geoapi',
+    }
+
+    SWAGGER_UI_JSONEDITOR = True
+
+
+class DevelopmentConfig(BaseConfig):
+    """config for DevelopmentConfig."""
+    DEBUG = False
+    DEVELOPMENT = True
